faef0e20dc08c695ba0f9ece3130a3c0d04cacfd
robensam17 <robbu@live.nl>
2020-06-23 15:06:08 +0200

    Update game.js

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ var level = 0;
 
 $(document).click(function() {
   if (!started) {
+    window.scrollTo(0, 1);
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
